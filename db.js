8e96e4ce3b4185c732dfb796e11591c2ef19a39b
Vy <nvy7650@gmail.com>
2026-05-21 18:43:27 +0700

    update login api

--- db.js
+++ db.js
@@ -1,14 +1,31 @@
 const mysql = require("mysql2");
+
 require("dotenv").config();
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection(
+  process.env.DATABASE_URL
+);
 
 db.connect((err) => {
+
   if (err) {
-    console.log("❌ MySQL connection error:", err);
+
+    console.log(
+      "❌ MySQL connection error:",
+      err
+    );
+
   } else {
+
     console.log("✅ MySQL connected!");
+
+    // VIETNAM TIME
+    db.query(
+      "SET time_zone = '+07:00'"
+    );
+
   }
+
 });
 
 module.exports = db;
