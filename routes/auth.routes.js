a1644a6e1c7a550e381cc97d977fcfac0487e515
Vy <nvy7650@gmail.com>
2026-05-21 17:35:44 +0700

    add login api

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -16,6 +16,67 @@ router.post("/organizer/register", (req, res) => {
     password,
   } = req.body;
 
+  const checkSql = `
+  SELECT * FROM users
+  WHERE email = ? OR phone = ?
+`;
+
+db.query(
+  checkSql,
+  [email, phone],
+  (checkErr, checkResult) => {
+
+    if (checkErr) {
+      return res.status(500).json(checkErr);
+    }
+
+    if (checkResult.length > 0) {
+
+      return res.status(400).json({
+        message: "Email hoặc số điện thoại đã tồn tại",
+      });
+
+    }
+
+    const sql = `
+      INSERT INTO users
+      (
+        name,
+        email,
+        phone,
+        password,
+        role,
+        status
+      )
+      VALUES (?, ?, ?, ?, ?, ?)
+    `;
+
+    db.query(
+      sql,
+      [
+        organization_name,
+        email,
+        phone,
+        password,
+        "ORGANIZER",
+        "ACTIVE",
+      ],
+      (err, result) => {
+
+        if (err) {
+          return res.status(500).json(err);
+        }
+
+        res.json({
+          message: "Đăng ký organizer thành công",
+        });
+
+      }
+    );
+
+  }
+);
+
   const sql = `
     INSERT INTO users
     (
@@ -29,26 +90,44 @@ router.post("/organizer/register", (req, res) => {
     VALUES (?, ?, ?, ?, ?, ?)
   `;
 
+
+});
+
+// LOGIN
+router.post("/login", (req, res) => {
+
+  const {
+    email,
+    password,
+  } = req.body;
+
+  const sql = `
+    SELECT *
+    FROM users
+    WHERE email = ?
+    AND password = ?
+  `;
+
   db.query(
     sql,
-    [
-      organization_name,
-      email,
-      phone,
-      password,
-      "ORGANIZER",
-      "ACTIVE",
-    ],
-    (err, result) => {
+    [email, password],
+    (err, results) => {
 
       if (err) {
-
         return res.status(500).json(err);
+      }
+
+      if (results.length === 0) {
+
+        return res.status(401).json({
+          message: "Sai email hoặc mật khẩu",
+        });
 
       }
 
       res.json({
-        message: "Đăng ký organizer thành công",
+        message: "Đăng nhập thành công",
+        user: results[0],
       });
 
     }
