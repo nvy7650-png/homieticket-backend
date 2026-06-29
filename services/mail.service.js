549fd9128914065b0129055138a2b4488673a6e9
Vy <nvy7650@gmail.com>
2026-06-29 14:39:59 +0700

    add organizer sidebar layout

--- services/mail.service.js
+++ services/mail.service.js
@@ -18,29 +18,18 @@ const transporter = nodemailer.createTransport({
   debug: true,
 });
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: false,
-
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
-
 async function sendTestMail(email) {
   await transporter.sendMail({
-  from: '"HomieTicket" <violet200204@gmail.com>',
-  to: email,
+    from: '"HomieTicket" <violet200204@gmail.com>',
+    to: email,
 
-  subject: "HOMIETICKET TEST",
+    subject: "HOMIETICKET TEST",
 
-  html: `
-    <h2>🎉 HOMIETICKET</h2>
-    <p>Gửi mail thành công bằng Brevo.</p>
-  `,
-});
+    html: `
+      <h2>🎉 HOMIETICKET</h2>
+      <p>Gửi mail thành công bằng Brevo.</p>
+    `,
+  });
 }
 
 module.exports = {
