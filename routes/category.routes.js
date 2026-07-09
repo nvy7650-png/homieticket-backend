eb0ec744c97e700f78d1a2aaaeeb083bd3d071d6
Vy <nvy7650@gmail.com>
2026-07-09 21:48:46 +0700

    install cloudinary

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -26,4 +26,35 @@ router.get("/", (req, res) => {
 
 });
 
+// UPDATE CATEGORY
+router.put("/:id", (req, res) => {
+
+  const { name, description } = req.body;
+
+  const sql = `
+    UPDATE categories
+    SET
+      name = ?,
+      description = ?
+    WHERE id = ?
+  `;
+
+  db.query(
+    sql,
+    [name, description, req.params.id],
+    (err, result) => {
+
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json({
+        message: "Cập nhật danh mục thành công"
+      });
+
+    }
+  );
+
+});
+
 module.exports = router;
