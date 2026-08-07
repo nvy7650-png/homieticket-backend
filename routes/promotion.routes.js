2c1e1d73b11fddb2fd74f25bdf7e61191f2baebc
Vy <nvy7650@gmail.com>
2026-08-07 21:11:35 +0700

    install cloudinary

--- routes/promotion.routes.js
+++ routes/promotion.routes.js
@@ -109,21 +109,7 @@ router.post(
 
     } = req.body;
 
-    const promo = rows[0];
-   const now = new Date(
-  new Date().toLocaleString("en-US", {
-    timeZone: "Asia/Ho_Chi_Minh",
-  })
-);
-
-    console.log("========== APPLY ==========");
-console.log("NOW:", now);
-console.log("START:", promo.start_date);
-console.log("END:", promo.end_date);
-console.log("USED:", promo.used_count);
-console.log("QTY:", promo.quantity);
-console.log("TOTAL:", total_price);
-console.log("MIN:", promo.min_order_value);
+   
 let status = "INACTIVE";
 
 if (
@@ -817,8 +803,14 @@ router.post(
         const promo =
           rows[0];
 
-        const now =
-          new Date();
+        const now = new Date(
+  new Date().toLocaleString("en-US", {
+    timeZone: "Asia/Ho_Chi_Minh",
+  })
+);
+console.log("NOW:", now);
+console.log("START:", new Date(promo.start_date));
+console.log("END:", new Date(promo.end_date));
 
         // =======================
         // HẾT HẠN
